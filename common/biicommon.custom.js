0c8f2f6617a577aa5480b38fc41a8710c44a7a67
dn <nhat.doan.expo@gmail.com>
2025-05-13 20:23:48 +0700

    up

--- common/biicommon.custom.js
+++ common/biicommon.custom.js
@@ -276,7 +276,7 @@
     }()); // End snowFall module
 
     // --- Global Variables & Configuration (from `biicore` object) ---
-    var SNOW_Picture = '/common/imgs/heart.png';
+    var SNOW_Picture = './common/imgs/heart.png';
     var special_custom = ['646f6e3d778825e6f306667f', '64a04f6beb89a210fc07656a']; // Specific template IDs?
 
     // --- Initialize Snow Effect on Page Load ---
